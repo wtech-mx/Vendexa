9ce3e73b396b498d3646b1c42af9ff1f2c432739
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-11-21 18:19:30 -0600

    Actualizacon de fecha y formateando en español las validaciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $dateFormat = 'd/m/Y';
+
     /**
      * The attributes that are mass assignable.
      *
