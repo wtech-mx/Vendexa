df818760ebefd760fb803ea847c2fe1186008986
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-12-01 21:47:34 -0600

    Actualzaicion correcion de eororr de overflow

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,7 +11,7 @@
 
 <section class="products bg_product ">
 
-    <div class="row z-1 position-relative">
+    <div class="row z-1 position-relative px-3 px-md-4 px-xl-5">
 
         <div class="col-12">
             <h2 class="tiitle_modal_dark text-center mt-3 mb-3">Productos</h2>
@@ -117,6 +117,7 @@
 
             </div>
         </div>
+
 
     </div>
 
