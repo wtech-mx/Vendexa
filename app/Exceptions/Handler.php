61caa2d48586fe0baedbc50b4411498d7c00c564
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-12-06 20:47:16 -0600

    Actualizacion de envio de erroes por correo

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,47 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function report(Throwable $exception)
+    {
+        if ($this->shouldReport($exception)) {
+            $message = $exception->getMessage();
+            $statusCode = method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500;
+            $subject = "Error $statusCode: $message";
+
+            if (app()->bound('sentry') && $this->shouldReportToSentry($exception)) {
+                app('sentry')->captureException($exception);
+            }
+
+            if (app()->environment('production')) {
+                $this->sendEmail($subject, $exception);
+            }
+        }
+
+        parent::report($exception);
+    }
+
+    protected function sendEmail($subject, $exception)
+    {
+        $webpage = WebPage::first();
+
+        $to = "josue.adrian.ramirezhernandez@gmail.com";
+        $to2  = "dayanna.wtech@gmail.com";
+
+        $message = $exception->getMessage() . "\n\n" .
+            $exception->getTraceAsString() . "\n\n" .
+            'Request Data: ' . json_encode(request()->all());
+
+        // Mail::raw($message, function ($email) use ($to, $subject) {
+        //     $email->to($to)
+        //           ->subject($subject);
+        // });
+
+        Mail::raw($message, function ($email) use ($to, $to2, $subject) {
+            $email->to($to)
+                  ->to($to2)
+                  ->subject($subject);
+        });
+
+    }
 }
