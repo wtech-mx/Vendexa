6aa47b388e87cf53d55385d7baa092499f9975de
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-12-08 21:43:19 -0600

    Actualizacion del scaner

--- app/Http/Controllers/ScannerController.php
+++ app/Http/Controllers/ScannerController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorias;
+use App\Models\Marcas;
+use App\Models\Proveedores;
+use App\Models\SubCategorias;
 use Illuminate\Http\Request;
+
+
 
 class ScannerController extends Controller
 {
     public function index(){
 
-        return view('scanner.index');
+        $user = auth()->user()->id_empresa;
+        $proveedores = Proveedores::where('id_empresa', $user)->get();
+        $marcas = Marcas::where('id_empresa', $user)->get();
+        $categorias = Categorias::where('id_empresa', $user)->get();
+        $subcategorias = SubCategorias::where('id_empresa', $user)->get();
+
+        return view('scanner.index', compact('proveedores', 'marcas', 'categorias', 'subcategorias'));
     }
 }
