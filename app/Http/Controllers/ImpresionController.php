43f088d98f72bc803278b3c01186ddec4bae6195
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-01-16 12:42:46 -0600

    CATALOGO PDF RECUPERADO

--- app/Http/Controllers/ImpresionController.php
+++ app/Http/Controllers/ImpresionController.php
@@ -27,4 +27,38 @@ class ImpresionController extends Controller
         return $pdf->download('etiqueta_'.$sku.'.pdf');
 
     }
+
+    public function pdf_productos(Request $request){
+
+        $productosSeleccionados = $request->get('productos');
+
+        $products = []; // Inicializar un arreglo vacío para almacenar los productos
+        $fechaActual = date('Y-m-d');
+
+        foreach ($productosSeleccionados as $producto) {
+            $product = Productos::where('id', $producto)->first(); // Obtener el producto por SKU
+            if ($product) {
+                $products[] = $product; // Agregar el producto al arreglo
+            }
+        }
+
+
+        $pdf_productos = public_path() . '/pdf';
+
+        $path = $pdf_productos;
+
+        $pdf = PDF::loadView('pdf.catalogo_productos', ['productos' => $products]);
+
+        // Guardar el PDF temporalmente
+        $pdfPath = public_path('pdfs/productos_' . $fechaActual . '.pdf');
+
+
+        $pdf->save($pdfPath);
+
+        // Obtener la URL del PDF almacenado temporalmente
+        $pdfUrl = asset('pdfs/productos_' . $fechaActual . '.pdf');
+
+        return response()->json(['url' => $pdfUrl], 200); // Enviar la URL del PDF como respuesta
+
+    }
 }
