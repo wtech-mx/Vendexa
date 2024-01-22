5302222294abf74acd5e2ab7550d9e921d9c9077
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-01-22 10:30:31 -0600

    Mini actualizacion cards modulos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -122,21 +122,19 @@
                 <div class="card_dashboard p-2">
 
                     <div class="card_img">
-                        <a href="">
-                            <img class="img_icon_dash" src="{{ asset('assets/media/icons/scanner.webp') }}" alt="">
+                        <a href="{{ route('productos.index') }}">
+                            <img class="img_icon_dash" src="{{ asset('assets/media/icons/corte.webp') }}" alt="">
                         </a>
                     </div>
 
 
-                    <h6 class="tittle_card_dash text-center mt-3 mb-3">Scanner</h6>
+                    <h6 class="tittle_card_dash text-center mt-3 mb-3">Corte</h6>
 
-                    <div class="d-flex justify-content-center">
-                        <a type="button" class="btn_primary_blue_dash" data-bs-toggle="modal" data-bs-target="#show_Scanner">
-                            Acceder
-                        </a>
-                    </div>
+                    <a href="{{ route('productos.index') }}" class="btn_primary_blue_dash">Acceder </a>
 
-                    </p>
+                    <a type="button" class="btn_plus_dash" data-bs-toggle="modal" data-bs-target="#creatProduct">
+                        <img class="img_plus_dash" src="{{ asset('assets/media/icons/anadir_white.webp') }}" alt="">
+                    </a>
                 </div>
             </div>
 
@@ -242,6 +240,25 @@
                 <div class="card_dashboard p-2">
 
                     <div class="card_img">
+                        <a href="{{ route('orders.index') }}">
+                            <img class="img_icon_dash" src="{{ asset('assets/media/icons/charts-imageonline.co-6846928.webp') }}" alt="">
+                        </a>
+                    </div>
+
+
+                    <h6 class="tittle_card_dash text-center mt-3 mb-3">Reportes</h6>
+
+                    <div class="d-flex justify-content-center">
+                        <a href="{{ route('orders.index') }}" class="btn_primary_blue_dash">Acceder </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-6 col-xs-6 col-sm-6 col-md-6-col-xl-4 mb-3 d-flex justify-content-center animation_card">
+                <div class="card_dashboard p-2">
+
+                    <div class="card_img">
                         <a href="{{ route('clientes.index') }}">
                             <img class="img_icon_dash" src="{{ asset('assets/media/icons/roles_permisos.webp') }}" alt="">
                         </a>
@@ -274,6 +291,28 @@
                     </div>
                 </div>
             </div>
+
+            {{-- <div class="col-6 col-xs-6 col-sm-6 col-md-6-col-xl-4 mb-3 d-flex justify-content-center animation_card">
+                <div class="card_dashboard p-2">
+
+                    <div class="card_img">
+                        <a href="">
+                            <img class="img_icon_dash" src="{{ asset('assets/media/icons/scanner.webp') }}" alt="">
+                        </a>
+                    </div>
+
+
+                    <h6 class="tittle_card_dash text-center mt-3 mb-3">Scanner</h6>
+
+                    <div class="d-flex justify-content-center">
+                        <a type="button" class="btn_primary_blue_dash" data-bs-toggle="modal" data-bs-target="#show_Scanner">
+                            Acceder
+                        </a>
+                    </div>
+
+                    </p>
+                </div>
+            </div> --}}
 
         <!--<div class="col-6 col-xs-6 col-sm-6 col-md-6-col-xl-4 mb-3 d-flex justify-content-center animation_card">
                 <div class="card_dashboard p-2">
