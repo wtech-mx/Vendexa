b8fd28b69f1321ae9f443b94521878eed990d19a
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-01-23 14:20:48 -0600

    Actualizacion de porductos vendidos

--- resources/views/reportes/components/ventas_productos.blade.php
+++ resources/views/reportes/components/ventas_productos.blade.php
@@ -1,7 +1,7 @@
 <div class="accordion-body row" style="margin: 0!important;padding: 0!important;">
 
     <div class="form-group col-12 px-4 py-3">
-        <h3 for="name" class="label_custom_primary_product mb-2"><strong>Ingresos</strong></h3>
+        <h3 for="name" class="label_custom_primary_product mb-2"><strong>Productos</strong></h3>
     </div>
 
     <div class="form-group col-12 px-4 ">
@@ -28,17 +28,55 @@
         </div>
     </div>
 
-    <div class="form-group col-0 col-sm-0 col-md-0 col-lg-6 col-xl-6 px-4 py-3">
+    <div class="form-group col-auto col-lg-6 px-0 py-3">
+
+    </div>
+
+    <div class="form-group col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 px-4 py-3">
+        <h3 for="name" class="label_custom_primary_product mb-4">Productos Mas vendidos</h3>
+
+
+        <div class="comtainer_products_width row">
+
+            <div class="col-6 col-sm-6 col-md-6 col-lg-9 my-auto">
+                <img class="img_prodcut_reportes d-inline" src="{{ asset('assets/media/icons/calendar-dar.webp') }}" alt="" >
+                <h5 class="d-inline text_titlle_tab_reporte">NK300</h5>
+            </div>
+
+            <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+                <h6 class="text_subtitlle_tab_reporte">SKU : 1685513</h6>
+                <h5 class="text_titlle_tab_reporte">100 Ventas</h5>
+                <h6 class="text_subtitlle_tab_reporte">STOCK : 55</h6>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="form-group col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 px-4 py-3">
+        <h3 for="name" class="label_custom_primary_product mb-4 ">Productos Menos vendidos</h3>
+
+        <div class="comtainer_products_width row">
+
+            <div class="col-6 col-sm-6 col-md-6 col-lg-9 my-auto">
+                <img class="img_prodcut_reportes d-inline" src="{{ asset('assets/media/icons/calendar-dar.webp') }}" alt="" >
+                <h5 class="d-inline text_titlle_tab_reporte">NK300</h5>
+            </div>
+
+            <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+                <h6 class="text_subtitlle_tab_reporte">SKU : 1685513</h6>
+                <h5 class="text_titlle_tab_reporte">100 Ventas</h5>
+                <h6 class="text_subtitlle_tab_reporte">STOCK : 55</h6>
+            </div>
+
+        </div>
 
     </div>
 
 
-    <div class="form-group col-auto px-0 py-3">
-
-    </div>
 
     <div class="form-group col-12 px-4 py-3 mb-3">
-        <h3 for="name" class="label_custom_primary_product mb-4"> <img src="{{ asset('assets/media/icons/bolsa-de-dinero.webp') }}" alt="" class="img_card_head_reportes"> Total <strong>$70,000.0</strong> </h3>
+        <h3 for="name" class="label_custom_primary_product mb-4"> <img src="{{ asset('assets/media/icons/retail.webp') }}" alt="" class="img_card_head_reportes"> Total <strong>500 Productos</strong> </h3>
 
         <a href="{{ route('orders.index') }}" class="btn_primary_blue_dash py-2">Generara Reporte  <img src="{{ asset('assets/media/icons/pdf.webp') }}" alt="" style="width: 23px"></a>
     </div>
